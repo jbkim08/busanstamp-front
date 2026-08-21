dbd64fdb7ccf2b7c3f11cb68e0167c500c682460
KJB <drv98@naver.com>
2026-08-21 15:29:19 +0900

    16. Router 수정

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,25 +1,43 @@
 import { Route, Routes } from "react-router";
+
+import AdminRoute from "../components/auth/AdminRoute";
+import ProtectedRoute from "../components/auth/ProtectedRoute";
 import MainLayout from "../components/layout/MainLayout";
+
+import AdminHomePage from "../pages/AdminHomePage";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
+import MyPage from "../pages/MyPage";
 import NotFoundPage from "../pages/NotFoundPage";
 import PlaceDetailPage from "../pages/PlaceDetailPage";
 import PlaceListPage from "../pages/PlaceListPage";
 import SignupPage from "../pages/SignupPage";
 
-//전체 애플리케이션의 경로를 정의 ( 공통 레이아웃 Mainlayout 적용 )
 function AppRouter() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
+        {/* 공개 페이지 */}
         <Route index element={<HomePage />} />
 
         <Route path="login" element={<LoginPage />} />
+
         <Route path="signup" element={<SignupPage />} />
 
         <Route path="places">
           <Route index element={<PlaceListPage />} />
+
           <Route path=":placeId" element={<PlaceDetailPage />} />
+        </Route>
+
+        {/* 로그인 사용자 전용 */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="mypage" element={<MyPage />} />
+        </Route>
+
+        {/* 관리자 전용 */}
+        <Route element={<AdminRoute />}>
+          <Route path="admin" element={<AdminHomePage />} />
         </Route>
 
         <Route path="*" element={<NotFoundPage />} />
