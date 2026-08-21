fcce9e9d8f58e766e6555bb64c792576d7e792e3
KJB <drv98@naver.com>
2026-08-21 10:15:31 +0900

    20. main.jsx 설정

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,24 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 30 * 1000, //30초 동안에는 같은 요청을 하지 않음
+      retry: 1, //실패시 1번 더 다시 시도
+      refetchOnWindowFocus: false, //화면을 다른데 다녀와도 자동 요청을 하지 않음
+    },
+  },
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 );
