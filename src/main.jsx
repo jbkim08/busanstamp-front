11f20c7df67d49bc965bade1628b7c2e220e6593
KJB <drv98@naver.com>
2026-08-21 15:31:38 +0900

    17. main.jsx에 인증 초기화 적용

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthInitializer from "./components/auth/AuthInitializer.jsx";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,7 +20,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <AuthInitializer>
+          <App />
+        </AuthInitializer>
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
