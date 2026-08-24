76b094fd1c999a5acebadd26d23da4985480bb2f
KJB <drv98@naver.com>
2026-08-24 15:36:15 +0900

    11. Router 추가

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -14,6 +14,7 @@ import SignupPage from "../pages/SignupPage";
 import AdminPlaceListPage from "../pages/admin/AdminPlaceListPage";
 import AdminPlaceFormPage from "../pages/admin/AdminPlaceFormPage";
 import KakaoSearchPage from "../pages/admin/KakaoSearchPage";
+import MapPage from "../pages/MapPage";
 
 function AppRouter() {
   return (
@@ -25,6 +26,8 @@ function AppRouter() {
         <Route path="login" element={<LoginPage />} />
 
         <Route path="signup" element={<SignupPage />} />
+
+        <Route path="map" element={<MapPage />} />
 
         <Route path="places">
           <Route index element={<PlaceListPage />} />
