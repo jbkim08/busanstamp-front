5d7443f26c6e97cf9607dfb3a6ffe158d132193d
KJB <drv98@naver.com>
2026-08-24 15:07:53 +0900

    카카오검색 추가

--- src/components/admin/AdminPageHeader.jsx
+++ src/components/admin/AdminPageHeader.jsx
@@ -12,16 +12,31 @@ function AdminPageHeader({ title, description, buttonText, buttonLink }) {
       </div>
 
       {buttonText && buttonLink && (
-        <Link
-          to={buttonLink}
-          className="
-            rounded-xl bg-violet-600 px-5 py-3
-            font-semibold text-white transition
+        <div className="mb-6 flex justify-end gap-3">
+          <Link
+            to="/admin/kakao-search"
+            className="
+            rounded-xl bg-amber-400
+            px-5 py-3 font-semibold
+            text-slate-900
+            transition hover:bg-amber-300
+          "
+          >
+            카카오에서 장소 찾기
+          </Link>
+
+          <Link
+            to="/admin/places/new"
+            className="
+            rounded-xl bg-violet-600
+            px-5 py-3 font-semibold
+            text-white
             hover:bg-violet-500
           "
-        >
-          {buttonText}
-        </Link>
+          >
+            직접 등록
+          </Link>
+        </div>
       )}
     </div>
   );
